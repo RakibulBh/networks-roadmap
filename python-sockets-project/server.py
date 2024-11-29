735594cabbca6ba23c8d5a9a08189e539d3f9aff
RakibulBh <rakibulbhuiyan1973@gmail.com>
2024-11-29 19:13:34 +0000

    add: server code implemented

--- python-sockets-project/server.py
+++ python-sockets-project/server.py
@@ -1,2 +1,41 @@
 import socket
 import threading
+
+
+HEADER = 64
+PORT = 5050
+# SERVER = "192.168.1.173"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg_length = int(conn.recv(HEADER).decode(FORMAT));
+        msg = conn.recv(msg_length).decode(FORMAT)
+        if msg ==  DISCONNECT_MESSAGE:
+            connected = False
+
+        print(f"[{addr}] {msg}")
+
+    conn.close()
+
+def start():
+    server.listen()
+    print(f"[SERVER] listening on server {SERVER} and port {PORT}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start();
+print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+
+print("[STARTING] server is starting...")
+start()
